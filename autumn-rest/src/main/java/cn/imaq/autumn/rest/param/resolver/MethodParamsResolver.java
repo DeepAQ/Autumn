5606f3ebc921534383c46ac122b61eb6185b60fb
DeepAQ <i@aq741.com>
2018-02-04 23:32:08 +0800

    Method param resolve (2/3) multiple type converting

--- autumn-rest/src/main/java/cn/imaq/autumn/rest/param/resolver/MethodParamsResolver.java
+++ autumn-rest/src/main/java/cn/imaq/autumn/rest/param/resolver/MethodParamsResolver.java
@@ -2,6 +2,7 @@ package cn.imaq.autumn.rest.param.resolver;
 
 import cn.imaq.autumn.rest.exception.ParamConvertException;
 import cn.imaq.autumn.rest.exception.ParamResolveException;
+import cn.imaq.autumn.rest.param.converter.CollectionConverter;
 import cn.imaq.autumn.rest.param.converter.TypeConverter;
 import cn.imaq.autumn.rest.param.value.ParamValue;
 import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
@@ -9,15 +10,13 @@ import io.github.lukehutch.fastclasspathscanner.FastClasspathScanner;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.annotation.Annotation;
-import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
+import java.lang.reflect.*;
+import java.util.*;
 
 public class MethodParamsResolver {
     private static Map<Class<? extends Annotation>, AnnotatedParamResolver> annotatedResolvers = new HashMap<>();
     private static Map<Class<?>, TypeConverter> typeConverters = new HashMap<>();
+    private static CollectionConverter collectionConverter = new CollectionConverter();
 
     private static void addAnnotatedResolver(AnnotatedParamResolver<?> resolver) {
         annotatedResolvers.put(resolver.getAnnotationClass(), resolver);
@@ -71,17 +70,31 @@ public class MethodParamsResolver {
             ParamValue value = resolver.resolve(param, req, resp);
             Class<?> paramType = param.getType();
             boolean needMultipleValues = paramType.isArray() || Collection.class.isAssignableFrom(paramType);
+            // get raw value before converting
             Object rawValue = needMultipleValues ? value.getMultipleValues() : value.getSingleValue();
             try {
                 if (rawValue != null && !paramType.isAssignableFrom(rawValue.getClass())) {
                     // convert types
                     if (needMultipleValues) {
-                        // TODO convert multiple values
+                        Collection valueCollection = ((Collection) rawValue);
+                        if (paramType.isArray()) {
+                            Class<?> innerType = paramType.getComponentType();
+                            rawValue = convertMultiple(valueCollection, innerType);
+                        } else {
+                            Type type = param.getParameterizedType();
+                            if (type instanceof ParameterizedType) {
+                                Type innerType = ((ParameterizedType) type).getActualTypeArguments()[0];
+                                if (innerType instanceof Class) {
+                                    valueCollection = Arrays.asList(convertMultiple(valueCollection, (Class) innerType));
+                                }
+                            }
+                            rawValue = collectionConverter.convert(valueCollection, (Class<? extends Collection>) paramType);
+                        }
                     } else {
                         rawValue = convertSingle(rawValue, paramType);
                     }
                 }
-            } catch (ParamConvertException e) {
+            } catch (Exception e) {
                 throw new ParamResolveException(e);
             }
             rawValues[i] = rawValue;
@@ -92,8 +105,30 @@ public class MethodParamsResolver {
     private <T> T convertSingle(Object src, Class<T> targetType) throws ParamConvertException {
         TypeConverter<T> converter = typeConverters.get(targetType);
         if (converter == null) {
-            throw new ParamConvertException("Unable to find converter for " + src.getClass().getName() + " to " + targetType.getName());
+            throw new ParamConvertException("Unable to find converter to " + targetType.getName());
         }
         return converter.convert(src, targetType);
+    }
+
+    private <T> T[] convertMultiple(Collection<?> src, Class<T> targetType) throws ParamConvertException {
+        TypeConverter<T> converter = null;
+        // List<T> results = new ArrayList<>(src.size());
+        T[] results = (T[]) Array.newInstance(targetType, src.size());
+        int index = 0;
+        for (Object o : src) {
+            if (targetType.isInstance(o)) {
+                results[index] = (T) o;
+            } else {
+                if (converter == null) {
+                    converter = typeConverters.get(targetType);
+                }
+                if (converter == null) {
+                    throw new ParamConvertException("Unable to find converter to " + targetType.getName());
+                }
+                results[index] = converter.convert(o, targetType);
+            }
+            index++;
+        }
+        return results;
     }
 }
