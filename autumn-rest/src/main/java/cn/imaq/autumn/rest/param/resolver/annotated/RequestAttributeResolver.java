1eeb4d1525eb5f76fa22392c531f952b27480b72
DeepAQ <i@aq741.com>
2019-03-30 00:14:09 +0800

    rest: fix RequestAttributeResolver

--- autumn-rest/src/main/java/cn/imaq/autumn/rest/param/resolver/annotated/RequestAttributeResolver.java
+++ autumn-rest/src/main/java/cn/imaq/autumn/rest/param/resolver/annotated/RequestAttributeResolver.java
@@ -1,6 +1,6 @@
 package cn.imaq.autumn.rest.param.resolver.annotated;
 
-import cn.imaq.autumn.rest.annotation.param.SessionAttribute;
+import cn.imaq.autumn.rest.annotation.param.RequestAttribute;
 import cn.imaq.autumn.rest.param.resolver.AnnotatedParamResolver;
 import cn.imaq.autumn.rest.param.value.ParamValue;
 import cn.imaq.autumn.rest.param.value.SingleValue;
@@ -9,9 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.lang.reflect.Parameter;
 
-public class RequestAttributeResolver extends AnnotatedParamResolver<SessionAttribute> {
+public class RequestAttributeResolver extends AnnotatedParamResolver<RequestAttribute> {
     @Override
-    protected ParamValue resolve(Parameter param, SessionAttribute anno, HttpServletRequest request, HttpServletResponse response) {
+    protected ParamValue resolve(Parameter param, RequestAttribute anno, HttpServletRequest request, HttpServletResponse response) {
         Object attr = request.getAttribute(anno.value());
         if (attr == null && anno.required()) {
             return new SingleValue<>(anno.defaultValue());
